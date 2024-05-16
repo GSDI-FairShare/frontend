0c38efbb6361b5780d716e161e400827cce2132d
Abraham Osco <aosco@fi.uba.ar>
2024-05-16 15:05:36 -0300

    Subo esqueleto del frontend

---
+++ fairshare-frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <h1>Iniciar seccion</h1>
+      
+
+    </main>
+  )
+}
+
+export default App
