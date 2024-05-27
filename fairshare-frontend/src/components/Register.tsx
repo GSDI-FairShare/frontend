58f68e7b83e1027bccd296ce7a338f613d690c8d
Kevin Gadacz <kgadacz@fi.uba.ar>
2024-05-27 20:49:59 -0300

    agrego logica para consumir endpoint de registro

--- fairshare-frontend/src/components/Register.tsx
+++ fairshare-frontend/src/components/Register.tsx
@@ -1,6 +1,7 @@
 import { Button, Stack, TextField } from "@mui/material";
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import "../login.css";
 
 export const Register = ({ toggleScreen }) => {
@@ -25,19 +26,39 @@ export const Register = ({ toggleScreen }) => {
     setNameUser(newInput);
   };
 
-  const handlerSubmit = (event) => {
+  const handlerSubmit = async (event) => {
     event.preventDefault();
-    if (inputEmail.includes('@') === false) {
-      setError({ activate: true, message: "Error: El email deberia incluir un @" });
+    if (!inputEmail.includes('@')) {
+      setError({ activate: true, message: "Error: El email debería incluir un @" });
       return;
     }
     if (inputPassword.length < 6) {
       setError({ activate: true, message: "Error: La contraseña debe ser mayor a 6 caracteres" });
       return;
     }
+    if (nameUser.trim() === "") {
+      setError({ activate: true, message: "Error: El nombre de usuario no puede estar vacío" });
+      return;
+    }
     setError({ activate: false, message: "" });
-    console.log("Enviados: inputEmail:", inputEmail, "inputPassword:", inputPassword);
-    navigate('/menu');
+
+    try {
+      const response = await axios.post('http://0.0.0.0:5000/signup', {
+        username: nameUser,
+        email: inputEmail,
+        password: inputPassword
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      console.log("Registro exitoso:", response.data);
+      navigate('/menu');
+    } catch (error) {
+      console.error("Error durante el registro:", error);
+      setError({ activate: true, message: "Error: No se pudo registrar el usuario. Por favor, inténtelo de nuevo." });
+    }
   };
 
   return (
@@ -47,7 +68,8 @@ export const Register = ({ toggleScreen }) => {
         <TextField id="inputNameUser" onChange={handlerUserName} label="Me llamo" variant="outlined" size='medium' />
         <TextField id="inputEmail" onChange={handlerEmail} label="Mi email" variant="outlined" />
         <TextField id="inputPassword" onChange={handlerPassword} type='password' label="Mi Contraseña" variant="outlined" />
-        <Button onClick={handlerSubmit} size='large' variant="contained">Crear cuenta</Button>
+        {error.activate && <p className='login__error'>{error.message}</p>}
+        <Button type="submit" size='large' variant="contained">Crear cuenta</Button>
         <Button onClick={() => toggleScreen('login')} size='large' variant="contained">Ya tienes una cuenta? 👉 Inicia sesión</Button>
       </Stack>
     </form>
