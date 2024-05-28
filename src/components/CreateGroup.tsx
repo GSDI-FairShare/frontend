c2c3a4fd74724bcaaef04d419cb6787a4c190e29
Angel Martin Veiga <amveiga@invap.com.ar>
2024-05-28 13:44:04 -0300

    se conecta al endpoint para crear grupo

--- src/components/CreateGroup.tsx
+++ src/components/CreateGroup.tsx
@@ -1,11 +1,12 @@
-// CreateGroup.js
 import React, { useState } from 'react';
 import { TextField, Button, IconButton, Grid, Typography } from '@mui/material';
 import { Add, Remove } from '@mui/icons-material';
+import axios from 'axios';
 
 export const CreateGroup = ({ addGroup, toggleScreen }) => {
   const [groupName, setGroupName] = useState('');
   const [emails, setEmails] = useState(['']);
+  const [error, setError] = useState(null);
 
   const handleEmailChange = (index, event) => {
     const newEmails = emails.slice();
@@ -23,11 +24,38 @@ export const CreateGroup = ({ addGroup, toggleScreen }) => {
     setEmails(newEmails);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    addGroup({ name: groupName, members: emails.filter(email => email) });
-    setGroupName('');
-    setEmails(['']);
+    const token = localStorage.getItem('token'); // Get the auth token from localStorage
+  
+    if (!token) {
+      setError("Error: Usuario no autenticado");
+      return;
+    }
+  
+    try {
+      const response = await axios.post('http://localhost:5000/groups', {
+        name: groupName,
+        members: emails.filter(email => email)  // Filter out empty emails
+      }, {
+        headers: {
+          'Authorization': `Bearer ${token}`  // Include the token in the Authorization header
+        }
+      });
+  
+      // Call addGroup to update the parent component or state
+      addGroup(response.data);
+      setGroupName('');
+      setEmails(['']);
+      setError(null); // Clear previous error messages
+    } catch (error) {
+      console.error("Error al crear el grupo:", error);
+      if (error.response && error.response.data) {
+        setError(error.response.data.detail);
+      } else {
+        setError("Error: No se pudo crear el grupo. Por favor, inténtelo de nuevo.");
+      }
+    }
   };
 
   return (
@@ -35,6 +63,7 @@ export const CreateGroup = ({ addGroup, toggleScreen }) => {
       <div>
         <h1>Crear un grupo</h1>
       </div>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
       <form style={{ marginTop: '0rem' }} onSubmit={handleSubmit}>
         <TextField
           label="Nombre del Grupo"
