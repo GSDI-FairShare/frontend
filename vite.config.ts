5a993d6df0a598ba6561ea309402d5882afdbdd2
Kevin Gadacz <kgadacz@fi.uba.ar>
2024-06-12 15:45:03 -0300

    especifico directorio de salida para deployar

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Especifica el nombre del directorio de salida
+  },
   define: {
     'process.env': {
       REACT_APP_API_URL: process.env.REACT_APP_API_URL,
